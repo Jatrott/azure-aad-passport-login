8b14dea8f42ca0be893e3964027cb6cd6ba88b6c
James Trott <jatrott@microsoft.com>
2019-08-20 15:32:24 -0700

    Working Hacked PoC of Passport auth with AAD and Web Login.

---
+++ app.js
@@ -0,0 +1,176 @@
+'use strict';
+require('dotenv').config()
+
+const restify = require('restify');
+const passport = require('passport');
+const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+const expressSession = require('express-session');
+const crypto = require('crypto');
+const querystring = require('querystring');
+//const https = require('https');
+const request = require('request');
+
+
+//oauth details
+const AZUREAD_APP_ID = process.env.AZUREAD_APP_ID;
+const AZUREAD_APP_PASSWORD = process.env.AZUREAD_APP_PASSWORD;
+const AZUREAD_APP_REALM = process.env.AZUREAD_APP_REALM;
+const AUTH_CALLBACKHOST = process.env.AUTH_CALLBACKHOST;
+const AUTH_STRATEGY = process.env.AUTH_STRATEGY;
+
+//=========================================================
+// Server Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3979, function () {
+  console.log('%s listening to %s', server.name, server.url); 
+});
+  
+console.log('Started...')
+var callbackURI = AUTH_CALLBACKHOST + '/api/OAuthCallback';
+console.log('MY CALLBACK: ' + callbackURI);
+
+//=========================================================
+// Auth Setup
+//=========================================================
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(expressSession({ secret: 'keyboard cat', resave: true, saveUninitialized: false }));
+server.use(passport.initialize());
+
+server.get('/login', function (req, res, next) {
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login', customState: req.query.address, resourceURL: process.env.MICROSOFT_RESOURCE }, function (err, user, info) {
+    console.log('login');
+    if (err) {
+      console.log(err);
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect('/login');
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.send('Welcome ' + req.user.displayName);
+      }
+    });
+  })(req, res, next);
+});
+
+server.get('/api/OAuthCallback',
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
+  (req, res) => {
+    console.log('OAuthCallback');
+    console.log(req);
+    const address = JSON.parse(req.query.state);
+    const magicCode = crypto.randomBytes(4).toString('hex');
+    const messageData = { magicCode: magicCode, accessToken: req.user.accessToken, refreshToken: req.user.refreshToken, userId: address.user.id, name: req.user.displayName, email: req.user.preferred_username };
+    
+    var continueMsg = new builder.Message().address(address).text(JSON.stringify(messageData));
+    console.log(continueMsg.toMessage());
+
+    res.send('Welcome ' + req.user.displayName + '! Please copy this number and paste it back to your chat so your authentication can complete: ' + magicCode);
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, id);
+});
+
+// Use the v2 endpoint (applications configured by apps.dev.microsoft.com)
+// For passport-azure-ad v2.0.0, had to set realm = 'common' to ensure works on azure app service
+var realm = AZUREAD_APP_REALM; 
+let oidStrategyv2 = {
+  redirectUrl: callbackURI,
+  realm: realm,
+  clientID: AZUREAD_APP_ID,
+  clientSecret: AZUREAD_APP_PASSWORD,
+  identityMetadata: 'https://login.microsoftonline.com/' + realm + '/v2.0/.well-known/openid-configuration',
+  skipUserProfile: false,
+  validateIssuer: false,
+  allowHttpForRedirectUrl: true,
+  responseType: 'code',
+  responseMode: 'query',
+  scope:['email', 'profile', 'offline_access', 'https://outlook.office.com/mail.read'],
+  passReqToCallback: true
+};
+
+// Use the v1 endpoint (applications configured by manage.windowsazure.com)
+// This works against Azure AD
+let oidStrategyv1 = {
+  redirectUrl: AUTH_CALLBACKHOST +'/api/OAuthCallback',
+  realm: realm,
+  clientID: AZUREAD_APP_ID,
+  clientSecret: AZUREAD_APP_PASSWORD,
+  validateIssuer: false,
+  //allowHttpForRedirectUrl: true,
+  oidcIssuer: undefined,
+  identityMetadata: 'https://login.microsoftonline.com/' + realm + '/.well-known/openid-configuration',
+  skipUserProfile: true,
+  responseType: 'code',
+  responseMode: 'query',
+  passReqToCallback: true
+};
+
+let strategy = null;
+if ( AUTH_STRATEGY == 'oidStrategyv1') {
+  strategy = oidStrategyv1;
+}
+if ( AUTH_STRATEGY == 'oidStrategyv2') {
+  strategy = oidStrategyv2;
+}
+
+passport.use(new OIDCStrategy(strategy,
+  (req, iss, sub, profile, accessToken, refreshToken, done) => {
+    if (!profile.displayName) {
+      return done(new Error("No oid found"), null);
+    }
+    // asynchronous verification, for effect...
+    process.nextTick(() => {
+      profile.accessToken = accessToken;
+      profile.refreshToken = refreshToken;
+      return done(null, profile);
+    });
+  }
+));
+
+function getAccessTokenWithRefreshToken(refreshToken, callback){
+  console.log("getAccessTokenWithRefreshToken");
+  var data = 'grant_type=refresh_token' 
+        + '&refresh_token=' + refreshToken
+        + '&client_id=' + AZUREAD_APP_ID
+        + '&client_secret=' + encodeURIComponent(AZUREAD_APP_PASSWORD) 
+
+  var options = {
+      method: 'POST',
+      url: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+      body: data,
+      json: true,
+      headers: { 'Content-Type' : 'application/x-www-form-urlencoded' }
+  };
+
+  request(options, function (err, res, body) {
+      if (err) return callback(err, body, res);
+      if (parseInt(res.statusCode / 100, 10) !== 2) {
+          if (body.error) {
+              return callback(new Error(res.statusCode + ': ' + (body.error.message || body.error)), body, res);
+          }
+          if (!body.access_token) {
+              return callback(new Error(res.statusCode + ': refreshToken error'), body, res);
+          }
+          return callback(null, body, res);
+      }
+      callback(null, {
+          accessToken: body.access_token,
+          refreshToken: body.refresh_token
+      }, res);
+  }); 
+}
+
